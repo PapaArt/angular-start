8206dca0e2e4d9a48666ac2b0fcc2bcd5c23ab65
Artur Papa <artur.papa@ufv.br>
2023-12-26 13:50:31 -0300

    add(+): up

---
+++ fairhouse/src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-list',
+  templateUrl: './housing-list.component.html',
+  styleUrls: ['./housing-list.component.css']
+})
+export class HousingListComponent {
+
+}
